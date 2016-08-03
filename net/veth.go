8dc0f03ea654d46e79dae7df61972a15373e9126
Martynas Pumputis <martynasp@gmail.com>
2016-08-03 17:21:36 +0100

    Use nsenter instead --netns-fd when calling weaveutil
    nsenter ensures that the entire Go runtime runs in the same
    network namespace.

--- net/veth.go
+++ net/veth.go
@@ -105,18 +105,18 @@ const (
 	vethPrefix = "v" + VethName // starts with "veth" to suppress UI notifications
 )
 
-func interfaceExistsInNamespace(ns netns.NsHandle, ifName string) bool {
-	_, err := WithNetNS(ns, "check-iface", ifName)
+func interfaceExistsInNamespace(nsPath string, ifName string) bool {
+	_, err := WithNetNS(nsPath, "check-iface", ifName)
 	return err == nil
 }
 
-func AttachContainer(ns netns.NsHandle, id, ifName, bridgeName string, mtu int, withMulticastRoute bool, cidrs []*net.IPNet, keepTXOn bool) error {
+func AttachContainer(ns netns.NsHandle, nsPath, id, ifName, bridgeName string, mtu int, withMulticastRoute bool, cidrs []*net.IPNet, keepTXOn bool) error {
 	ipt, err := iptables.New()
 	if err != nil {
 		return err
 	}
 
-	if !interfaceExistsInNamespace(ns, ifName) {
+	if !interfaceExistsInNamespace(nsPath, ifName) {
 		maxIDLen := IFNAMSIZ - 1 - len(vethPrefix+"pl")
 		if len(id) > maxIDLen {
 			id = id[:maxIDLen] // trim passed ID if too long
@@ -126,7 +126,7 @@ func AttachContainer(ns netns.NsHandle, id, ifName, bridgeName string, mtu int, 
 			if err := netlink.LinkSetNsFd(veth, int(ns)); err != nil {
 				return fmt.Errorf("failed to move veth to container netns: %s", err)
 			}
-			if _, err := WithNetNS(ns, "setup-iface", peerName, ifName); err != nil {
+			if _, err := WithNetNS(nsPath, "setup-iface", peerName, ifName); err != nil {
 				return fmt.Errorf("error setting up interface: %s", err)
 			}
 			return nil
